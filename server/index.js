206dccd2cdb46d1bd08832858c3c4cc6827e5bf4
kushagra2503 <yashyyy831@gmail.com>
2025-09-23 20:48:06 +0530

    OK

--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} New Request Received\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data) => {
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/":
-                res.end("Hello World");
+                if (req.method === "GET"){
+                    res.end("Home Page");
+                }
                 break;
             case "/about":
                 res.end("About Page");
                 break;
             default:
                 res.end("404 Not Found");
+                break;
+
+            case "/Signup" :
+                if (req.method === "GET"){
+                    res.end("Signup Page");
+                } else if (req.method === "POST"){
+                    res.end(" user wants to signup");
+                }
                 break;
         }
     });
