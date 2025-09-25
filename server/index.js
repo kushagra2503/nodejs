c97caf51ec7a8fc70c9c252939cd34eb168b772c
kushagra2503 <yashyyy831@gmail.com>
2025-09-25 23:00:53 +0530

    express

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
+const app = express(); // initialize express app 
+
+app.get("/", (req, res) => {
+    return res.send("Hello App");
+});
+
+app.get("/about", (req, res) => {
+    return res.send("Hello About");
+});
+
+function myHandler (req, res) {
     const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
@@ -29,8 +40,12 @@ const myServer = http.createServer((req, res) => {
                 break;
         }
     });
+};
+
+
+const myServer = http.createServer(app);
     
-});
+    
 
 myServer.listen(3000, () => {
     console.log("Server is running on port 3000");
