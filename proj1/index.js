e8d1c87aa1a692de33ccff2b4f345393317da671
kushagra2503 <yashyyy831@gmail.com>
2025-10-06 02:40:04 +0530

    Done

--- proj1/index.js
+++ proj1/index.js
@@ -8,10 +8,16 @@ app.get('/api/users',(req,res) =>{
     return res.json(users);
 });
 
-app.get('/api/users/:id',(req,res) =>{
+app.route('/api/users/:id').get((req,res) =>{
     const id = Number(req.params.id);
     const user = users.find((user)=> user.id === id);
     return res.json(user);
+}).post((req,res) =>{
+    res.json({status:"pending"})
+}).patch((req,res) =>{
+    res.json({status:"pending"})
+}).delete((req,res) =>{
+    res.json({status:"pending"})
 });
 
 
