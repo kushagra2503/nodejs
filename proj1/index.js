cc8a632b68bb5b6d338a68020bbc074ffb9333d8
kushagra2503 <yashyyy831@gmail.com>
2025-10-06 02:34:06 +0530

    KK

--- proj1/index.js
+++ proj1/index.js
@@ -8,6 +8,13 @@ app.get('/api/users',(req,res) =>{
     return res.json(users);
 });
 
+app.get('/api/users/:id',(req,res) =>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    return res.json(user);
+});
+
+
 app.get('/users',(req,res) => {
     const html = `
     <!DOCTYPE html>
